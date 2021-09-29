afdf3053b2990434f88170e81afadc95ca614029
kkgautam311 <45126741+kkgautam311@users.noreply.github.com>
2021-09-29 14:54:23 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname+"/date.js")
 const app = express();
 const port = 3000;
+
+var items = ["Buy food", "cook food", "eat food"];
+var workItem=[];
 
 app.set('view engine', 'ejs');
 
@@ -9,25 +13,58 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(express.static("public"));
+
 app.listen(port, function() {
   console.log('server is running on port ' + port);
 });
 
+
 app.get('/', function(req, res) {
 
-  var today = new Date();
-  var javaObj = {
-    0: 'Sunday',
-    1: 'Monday',
-    2: 'Tuesday',
-    3: 'Wednesday',
-    4: 'Thursday',
-    5: 'Friday',
-    6: 'Saturday'
-  }
-  var day = today.getDay();
-  
+  let day = date.getDay();
+
   res.render('list', {
-    day: javaObj[day]
+    day: day,
+    items: items
   })
 });
+
+
+app.post("/", function(req, res) {
+  var item = req.body.newItem
+
+  if (req.body.list==="Work"){
+    if (item !== "") {
+      workItem.push(item);
+    }
+    res.redirect('/work');
+  }else{
+    if (item !== "") {
+      items.push(item);
+    }
+    res.redirect('/');
+  }
+
+})
+
+
+app.get('/work',function(req,res){
+
+  res.render('list',{
+    day:'Work list',
+    items:workItem
+  });
+});
+
+app.post('/work',function(req,res){
+
+  let item=req.body.newItem
+  workItem.push(item)
+
+  res.redirect('/work');
+})
+
+app.get('/about',function(req,res){
+  res.render('about');
+})
